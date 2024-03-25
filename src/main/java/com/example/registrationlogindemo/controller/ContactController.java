8a0d689e3a3bad4a243c9c52f95903d173715858
Shazaam91 <sharzaan.1991@gmail.com>
2024-03-26 02:36:04 +0530

    Inquiry server side JSON change

--- src/main/java/com/example/registrationlogindemo/controller/ContactController.java
+++ src/main/java/com/example/registrationlogindemo/controller/ContactController.java
@@ -1,46 +1,29 @@
 package com.example.registrationlogindemo.controller;
 
 import com.example.registrationlogindemo.entity.ContactMessage;
-import com.example.registrationlogindemo.entity.User;
-import com.example.registrationlogindemo.repository.ContactMessageRepository;
 import com.example.registrationlogindemo.service.ContactMessageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Controller
+@RestController
 public class ContactController {
-
-    @Autowired
-    private ContactMessageRepository contactMessageRepository;
 
     private final ContactMessageService messageService;
 
-
-    @Autowired
     public ContactController(ContactMessageService messageService) {
         this.messageService = messageService;
-
     }
 
     @PostMapping("/contact")
-    public String submitContactForm(@RequestBody ContactMessage message) {
-        messageService.saveContactMessage(message);
-        return "redirect:/index"; // Redirect to homepage or any other page
+    public ResponseEntity<String> submitContactForm(@RequestBody ContactMessage message) {
+        try {
+            messageService.saveContactMessage(message);
+            return ResponseEntity.ok().body("{\"success\": true}");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"success\": false, \"error\": \"" + e.getMessage() + "\"}");
+        }
     }
-    @GetMapping("/inquiry")
-    public ResponseEntity<List<ContactMessage>> getAllContactMessage() {
-        List<ContactMessage> messageList = messageService.getAllContactMessage();
-        System.out.println("contactMessage" + messageList);
-        return new ResponseEntity<>(messageList, HttpStatus.OK);
-    }
-
-
 }
